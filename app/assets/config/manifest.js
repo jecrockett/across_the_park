f748635bbd04c82b8b7f75a71d4108fd5e85ed73
James Crockett <jecrockett@gmail.com>
2021-11-13 09:03:45 -0500

    Fix manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,3 @@
-// app/assets/config/manifest.js
-//
-//= link application.css
-//= link marketing.css
-//
-//= link application.js
-
-// link_tree ../images
-// link_directory ../javascripts .js
-// link_directory ../stylesheets .css
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
