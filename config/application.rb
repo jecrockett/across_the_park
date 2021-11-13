6d73fb3ce1b184267b160269d00191838be2fa6f
James Crockett <jecrockett@gmail.com>
2021-11-13 08:43:49 -0500

    Remove deprecated config

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module AcrossThePark
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Tell Rails to include fonts
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
