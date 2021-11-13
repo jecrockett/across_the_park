45bb5d7cf0086d273279ebbaf55dcd2548961b5d
James Crockett <jecrockett@gmail.com>
2021-11-13 08:40:28 -0500

    Add manifest.js file

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+// app/assets/config/manifest.js
+//
+//= link application.css
+//= link marketing.css
+//
+//= link application.js
+
+// link_tree ../images
+// link_directory ../javascripts .js
+// link_directory ../stylesheets .css
